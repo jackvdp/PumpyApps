25ae76e0568f3e3dad93d8d48dc1f919d32e8776
jackvdp <jack@pumpymusic.co.uk>
2023-03-08 23:13:57 +0000

    Add public init to maplabview

--- PumpyLibrary/PumpyLibrary/Views/Music Lab/MusicLabView.swift
+++ PumpyLibrary/PumpyLibrary/Views/Music Lab/MusicLabView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 public struct MusicLabView: View {
+    
+    public init() {}
+    
     public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
